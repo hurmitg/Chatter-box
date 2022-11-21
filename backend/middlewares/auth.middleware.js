66701a70dc8382596ecd711cd6e9dcf288f89eb0
Hurmit <Hurmitgrover7@gmail.com>
2022-11-22 02:16:29 +0530

    socket.io added

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -10,6 +10,7 @@ const protect = asyncHandler(async (req, res, next) => {
   ) {
     try {
       token = req.headers.authorization.split(" ")[1];
+      // console.log(token);
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await UserModel.findById(decoded.id).select("-password");
       next();
--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,43 @@ app.use("/api/message", messageRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(PORT, async () => {
+const server = app.listen(PORT, async () => {
   await connectDB();
   console.log(`server started on port ${PORT}`.blue.italic.underline);
+});
+
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", async (socket) => {
+  // console.log("connection established");
+
+  socket.on("setup", async (userData) => {
+    socket.join(userData._id);
+    // console.log(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("yes", room);
+  });
+
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (messageRecieved) => {
+    var chat = messageRecieved.chat;
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == messageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", messageRecieved);
+    });
+  });
 });
